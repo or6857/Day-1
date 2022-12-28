2a3d05b5ab87b82132b4bce9a404ef2f004dedbb
or6857 <or6857@srmist.edu.in>
2022-12-28 17:51:58 +0530

    third commit

--- day-1/src/components/SignupPage.js
+++ day-1/src/components/SignupPage.js
@@ -5,6 +5,18 @@ const SignupPage = () => {
   return (
     <div>
       <Navbar />
+      <div className="signup-container">
+        <form className="signup-form">
+          <p> Create your account </p>
+
+          <label>Your Name</label>
+          <input
+            onChange={(e) => setUserName(e.target.value)}
+            type="text"
+            placeholder="Your First and last Name"
+          />
+        </form>
+      </div>
     </div>
   );
 };
